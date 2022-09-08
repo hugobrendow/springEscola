b6e0d70caef30b70e1b04056d7e24b074f08e1e1
hugobrendow <hugobrendow97@gmail.com>
2022-09-08 09:21:50 -0300

    Adicionando comunicacao com API pública

---
+++ src/main/java/com/itau/escolaItauSpring/controller/CepController.java
@@ -0,0 +1,40 @@
+package com.itau.escolaItauSpring.controller;
+
+import com.itau.escolaItauSpring.dto.response.CepResponse;
+import com.itau.escolaItauSpring.service.CepFeignClient;
+import com.itau.escolaItauSpring.service.CepRestTemplateService;
+import com.itau.escolaItauSpring.service.CepWebClientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cep")
+@RequiredArgsConstructor
+public class CepController {
+
+    private final CepRestTemplateService cepRestTemplateService;
+    private final CepWebClientService cepWebClientService;
+    private final CepFeignClient cepFeignClient;
+
+    @GetMapping("/{cep}")
+    public ResponseEntity<CepResponse> listar(@PathVariable("cep") String cep) {
+        CepResponse cepResponse = cepRestTemplateService.getCep(cep);
+        return ResponseEntity.ok(cepResponse);
+    }
+
+    @GetMapping("/{cep}/web-client")
+    public ResponseEntity<CepResponse> listarWebClient(@PathVariable("cep") String cep) {
+        CepResponse cepResponse = cepWebClientService.getCep(cep);
+        return ResponseEntity.ok(cepResponse);
+    }
+
+    @GetMapping("/{cep}/feign")
+    public ResponseEntity<CepResponse> listarFeign(@PathVariable("cep") String cep) {
+        CepResponse resultado = cepFeignClient.getCep(cep);
+        return ResponseEntity.ok(resultado);
+    }
+}
