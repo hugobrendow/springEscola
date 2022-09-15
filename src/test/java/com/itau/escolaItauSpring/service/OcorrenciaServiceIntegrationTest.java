96c1ace10bbe723b912f680bc39973465c96c862
Sogolon <sogolonmvj@yahoo.com>
2022-09-15 20:26:42 -0300

    Test(OcorrenciaService): adicionou testes de integração!

--- src/test/java/com/itau/escolaItauSpring/service/OcorrenciaServiceIntegrationTest.java
+++ src/test/java/com/itau/escolaItauSpring/service/OcorrenciaServiceIntegrationTest.java
@@ -1,15 +1,15 @@
 package com.itau.escolaItauSpring.service;
 
-import com.itau.escolaItauSpring.mapper.OcorrenciaMapper;
+import com.itau.escolaItauSpring.dto.request.OcorrenciaRequest;
+import com.itau.escolaItauSpring.dto.response.OcorrenciaResponse;
 import com.itau.escolaItauSpring.model.Ocorrencia;
 import com.itau.escolaItauSpring.repository.OcorrenciaRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootTest
 class OcorrenciaServiceIntegrationTest {
@@ -21,27 +21,48 @@ class OcorrenciaServiceIntegrationTest {
     private OcorrenciaService ocorrenciaService;
 
     private static Ocorrencia ocorrencia;
+    private static OcorrenciaRequest ocorrenciaRequest;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    public static void setUp() {
         ocorrencia = new Ocorrencia();
-        ocorrencia.setDescricao("");
+        ocorrencia.setDescricao("Teste");
+        ocorrencia.setDataHora(LocalDateTime.parse("15-09-2022 19:45:00"));
+        ocorrencia.setAluno(null);
+
+        ocorrenciaRequest = new OcorrenciaRequest();
+        ocorrenciaRequest.setDescricao("Teste de requisição");
+        ocorrenciaRequest.setDataHora(LocalDateTime.parse("15-09-2022 19:45:00"));
+        ocorrenciaRequest.setAlunoId(null);
     }
 
     @AfterEach
     void tearDown() {
+        ocorrenciaRepository.deleteAll();
     }
 
+    @DisplayName("Busca as ocorrências no banco")
     @Test
     void buscarOcorrencias() {
+        ocorrenciaRepository.save(ocorrencia);
+        List<OcorrenciaResponse> ocorrenciaList = ocorrenciaService.buscarOcorrencias();
+
+        Assertions.assertEquals(1, ocorrenciaList.size());
     }
 
+    @DisplayName("Busca uma ocorrência no banco")
     @Test
     void buscarOcorrencia() {
+       Ocorrencia ocorrenciaLocal = ocorrenciaRepository.save(ocorrencia);
+       OcorrenciaResponse ocorrenciaResponse = ocorrenciaService.buscarOcorrencia(ocorrenciaLocal.getId());
+
+       Assertions.assertEquals(ocorrenciaLocal.getId(), ocorrenciaResponse.getId());
     }
 
+    @DisplayName("Registra uma ocorrência")
     @Test
     void registrarOcorrencia() {
+        OcorrenciaResponse ocorrenciaResponse = ocorrenciaService.registrarOcorrencia(ocorrenciaRequest);
     }
 
     @Test
