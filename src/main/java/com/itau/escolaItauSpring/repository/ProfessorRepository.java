9a85311db1ba57083e5680d4b46f823755d973d0
Bruno Augusto <b.augusto1501@gmail.com>
2022-09-15 12:35:39 -0300

    feat: criando integração professor (f005)

---
+++ src/main/java/com/itau/escolaItauSpring/repository/ProfessorRepository.java
@@ -0,0 +1,13 @@
+package com.itau.escolaItauSpring.repository;
+
+import com.itau.escolaItauSpring.model.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, UUID> {
+    List<Professor> findByNomeLikeIgnoreCase(String nome);
+}
