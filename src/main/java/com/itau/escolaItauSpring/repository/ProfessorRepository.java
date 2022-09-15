ac7db24f2824d228160de6c3aaa2d027621e5f73
Bruno Augusto <b.augusto1501@gmail.com>
2022-09-15 12:35:39 -0300

    Criação Professor Service

--- src/main/java/com/itau/escolaItauSpring/repository/ProfessorRepository.java
+++ src/main/java/com/itau/escolaItauSpring/repository/ProfessorRepository.java
@@ -4,9 +4,10 @@ import com.itau.escolaItauSpring.model.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor, UUID> {
-
+       List<Professor> findByNomeLikeIgnoreCase(String nome);
 }
