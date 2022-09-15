0d821e9373e6a74a604205ea005b551118b6226e
Davi Carrer <vittidavii@gmail.com>
2022-09-15 19:41:18 -0300

    Modificando AlunoResponse, TurmaResponse e MatriculaResponse para atender ao padrão de data

--- src/main/java/com/itau/escolaItauSpring/dto/response/MatriculaResponse.java
+++ src/main/java/com/itau/escolaItauSpring/dto/response/MatriculaResponse.java
@@ -1,5 +1,6 @@
 package com.itau.escolaItauSpring.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -13,10 +14,26 @@ public class MatriculaResponse {
     private Long numero;
 
     private Boolean status;
-
+    @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate data;
 
-    private AlunoResponse aluno;
+    private DadosAluno aluno;
 
-    private TurmaResponse turma;
+    private DadosTurma turma;
+
+    public class DadosAluno {
+        private UUID id;
+
+        private String nome;
+    }
+    public class DadosTurma {
+        private UUID id;
+        private String turno;
+        @JsonFormat(pattern="dd-MM-yyyy")
+        private LocalDate dataInicio;
+        @JsonFormat(pattern="dd-MM-yyyy")
+        private LocalDate dataFim;
+        private String codigoTurma;
+        private CursoResponse curso;
+    }
 }
--- src/main/java/com/itau/escolaItauSpring/dto/response/TurmaResponse.java
+++ src/main/java/com/itau/escolaItauSpring/dto/response/TurmaResponse.java
@@ -1,5 +1,6 @@
 package com.itau.escolaItauSpring.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -10,8 +11,12 @@ public class TurmaResponse {
     private UUID id;
     private String turno;
     private Integer numeroVagas;
+    @JsonFormat(pattern="dd-MM-yyyy")
     private LocalDate dataInicio;
+    @JsonFormat(pattern="dd-MM-yyyy")
     private LocalDate dataFim;
     private String codigoTurma;
     private CursoResponse curso;
+
+
 }
