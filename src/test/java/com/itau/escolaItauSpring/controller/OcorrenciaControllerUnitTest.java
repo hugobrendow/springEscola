fd71bee6b10ddd5ddec27626582235241ec8ec08
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-09-15 18:40:42 -0300

    test(controller): teste unitário deletar ocorrência

--- src/test/java/com/itau/escolaItauSpring/controller/OcorrenciaControllerUnitTest.java
+++ src/test/java/com/itau/escolaItauSpring/controller/OcorrenciaControllerUnitTest.java
@@ -19,6 +19,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -106,6 +107,17 @@ public class OcorrenciaControllerUnitTest {
         result.andExpect(status().isCreated());
         Assertions.assertEquals(expected, result.andReturn().getResponse().getContentAsString());
         Mockito.verify(ocorrenciaService, Mockito.times(1)).registrarOcorrencia(ArgumentMatchers.any(OcorrenciaRequest.class));
+    }
+
+    @DisplayName(value = "Deletar ocorrência por ID")
+    @Test
+    void testeDeletarOcorrencia() throws Exception {
+        doNothing().when(ocorrenciaService).deletarOcorrencia(any());
+
+        MvcResult result = this.mockMvc.perform(delete("/ocorrencia/{id}", UUID.fromString(idExistente)))
+                .andExpect(status().isNoContent())
+                .andReturn();
+        verify(ocorrenciaService, times(1)).deletarOcorrencia(UUID.fromString(idExistente));
     }
 
 }
