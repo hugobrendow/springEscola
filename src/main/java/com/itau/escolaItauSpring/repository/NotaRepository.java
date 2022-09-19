37ce817f1f056ca03b9abac8381804da86f1dfe8
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-09-19 11:23:34 -0300

    feat: Validacao de Nota

--- src/main/java/com/itau/escolaItauSpring/repository/NotaRepository.java
+++ src/main/java/com/itau/escolaItauSpring/repository/NotaRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface NotaRepository extends JpaRepository<Nota, UUID> {
     public List<Nota> findByMatriculaId(UUID id);
     public List<Nota> findByCursoDisciplinaId(UUID id);
+    public Optional<Nota> findByCursoDisciplinaIdAndMatriculaId(UUID cursoDisciplinaId, UUID matriculaId);
 }
