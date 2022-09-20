b85878982ac243534daabd305478722aa3c408d5
Ana Luísa Fogarin <ana.luisa.fogarin@gmail.com>
2022-09-20 13:12:16 -0300

    feat: criando controller, service, repository, mapper, request e response ProfessorCursoDisciplina

---
+++ src/main/java/com/itau/escolaItauSpring/controller/ProfessorCursoDisciplinaController.java
@@ -0,0 +1,27 @@
+package com.itau.escolaItauSpring.controller;
+
+import com.itau.escolaItauSpring.dto.request.VinculaCursoRequest;
+import com.itau.escolaItauSpring.dto.response.VinculaCursoResponse;
+import com.itau.escolaItauSpring.service.ProfessorCursoDisciplinaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/professor-curso-disciplina")
+@RequiredArgsConstructor
+public class ProfessorCursoDisciplinaController {
+
+    private final ProfessorCursoDisciplinaService professorCursoDisciplinaService;
+
+    @PostMapping("/vincula-curso")
+    public ResponseEntity<?> vincularCurso(@Valid @RequestBody VinculaCursoRequest vinculaCursoRequest){
+        VinculaCursoResponse vinculaCursoResponse = professorCursoDisciplinaService.vincularCurso(vinculaCursoRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(vinculaCursoResponse);
+    }
+
+}
---
+++ src/main/java/com/itau/escolaItauSpring/service/ProfessorCursoDisciplinaService.java
@@ -0,0 +1,38 @@
+package com.itau.escolaItauSpring.service;
+
+import com.itau.escolaItauSpring.dto.request.VinculaCursoRequest;
+import com.itau.escolaItauSpring.dto.response.VinculaCursoResponse;
+import com.itau.escolaItauSpring.exception.ItemNaoExistenteException;
+import com.itau.escolaItauSpring.mapper.ProfessorCursoDisciplinaMapper;
+import com.itau.escolaItauSpring.model.CursoDisciplina;
+import com.itau.escolaItauSpring.model.Professor;
+import com.itau.escolaItauSpring.model.ProfessorCursoDisciplina;
+import com.itau.escolaItauSpring.repository.ProfessorCursoDisciplinaRepository;
+import com.itau.escolaItauSpring.repository.ProfessorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProfessorCursoDisciplinaService {
+
+    private final ProfessorCursoDisciplinaRepository repository;
+    private final ProfessorRepository professorRepository;
+    private final CursoDisciplinaService cursoDisciplinaService;
+    private final ProfessorCursoDisciplinaMapper professorCursoDisciplinaMapper;
+
+    public VinculaCursoResponse vincularCurso(VinculaCursoRequest vinculaCursoRequest) {
+        Professor professor = professorRepository.findById(vinculaCursoRequest.getProfessorId()).orElseThrow(ItemNaoExistenteException::new);
+        CursoDisciplina cursoDisciplina = cursoDisciplinaService.buscarPorId(vinculaCursoRequest.getCursoDisciplinaId());
+
+        ProfessorCursoDisciplina professorCursoDisciplina = ProfessorCursoDisciplina.builder()
+                .professor(professor)
+                .cursoDisciplina(cursoDisciplina)
+                .titular(vinculaCursoRequest.getTitular())
+                .build();
+
+        ProfessorCursoDisciplina professorCursoDisciplinaSalvo = repository.save(professorCursoDisciplina);
+
+        return professorCursoDisciplinaMapper.toResponse(professorCursoDisciplinaSalvo);
+    }
+}
