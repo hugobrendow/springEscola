6ec4be7c5095c0ccc13ee3685e84f6c6821398ce
derik <degaloni@gmail.com>
2022-09-20 12:55:13 -0300

    Adicionado testes de Listar todas as notas, Buscar nota por ID

--- src/test/java/com/itau/escolaItauSpring/service/NotaServiceTests.java
+++ src/test/java/com/itau/escolaItauSpring/service/NotaServiceTests.java
@@ -2,6 +2,7 @@ package com.itau.escolaItauSpring.service;
 
 import com.itau.escolaItauSpring.dto.request.NotaRequest;
 import com.itau.escolaItauSpring.dto.response.NotaResponse;
+import com.itau.escolaItauSpring.exception.ItemNaoExistenteException;
 import com.itau.escolaItauSpring.exception.NotaJaCadastradaException;
 import com.itau.escolaItauSpring.mapper.NotaMapper;
 import com.itau.escolaItauSpring.model.CursoDisciplina;
@@ -16,16 +17,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class NotaServiceTests {
@@ -42,14 +41,19 @@ public class NotaServiceTests {
     @Mock
     private CursoDisciplinaService cursoDisciplinaService;
 
+
     private static UUID idCursoDisciplinaNotaCadastrada;
     private static UUID idMatriculaNotaCadastrada;
 
     private static UUID idCursoDisciplina;
     private static UUID idMatricula;
 
-    private static NotaRequest  notaRequestParametrosCadastrados;
-    private static NotaRequest  notaRequest;
+    private static NotaRequest notaRequestParametrosCadastrados;
+    private static NotaRequest notaRequest;
+
+    private UUID idNotaExistente;
+    private UUID idNotaNaoExistente;
+
 
     @BeforeEach
     public void init() {
@@ -66,6 +70,10 @@ public class NotaServiceTests {
         notaRequest = new NotaRequest();
         notaRequest.setCursoDisciplinaId(idCursoDisciplina);
         notaRequest.setMatriculaId(idMatricula);
+
+        idNotaNaoExistente = UUID.fromString("67fd945c-fb0a-414f-9ee5-42c8dffad754");
+        idNotaExistente = UUID.fromString("be82b537-713f-4e00-a3c6-a4a72653095e");
+
     }
 
     @Test
@@ -121,5 +129,50 @@ public class NotaServiceTests {
         assertEquals(response.getMatriculaId(), idMatricula);
         assertEquals(response.getCursoDisciplinaId(), idCursoDisciplina);
         verify(repository, times(1)).save(any());
+    }
+
+    @Test
+    @DisplayName("Pesquisar - pesquisa todas as notas cadastradas")
+    public void testListarTodasAsNotas() {
+
+        List<NotaResponse> notaResponses = List.of(new NotaResponse());
+        when(repository.findAll()).thenReturn(List.of(new Nota()));
+        when(mapper.toResponseList(any())).thenReturn(notaResponses);
+
+        List<NotaResponse> response = sut.listar();
+        assertEquals(notaResponses, response);
+        verify(repository, times(1)).findAll();
+
+    }
+
+    @Test
+    @DisplayName("Pesquisar - pesquisa notas através do Id")
+    public void testBuscarNotasPorId() {
+
+        when(repository.findById(idNotaExistente)).thenReturn(Optional.of(new Nota()));
+        when(mapper.toResponse(any())).thenReturn(new NotaResponse());
+
+        NotaResponse notaResponse = sut.buscarPorId(idNotaExistente);
+
+        assertNotNull(notaResponse);
+        verify(repository, times(1)).findById(idNotaExistente);
+    }
+
+    @Test
+    @DisplayName("Pesquisar - pesquisa notas através de um Id não existente")
+    public void testBuscarNotasPorIdInexistente() {
+
+        when(repository.findById(idNotaNaoExistente)).thenReturn(Optional.empty());
+
+        assertThrows(ItemNaoExistenteException.class,()->sut.buscarPorId(idNotaNaoExistente));
+        verify(repository, times(1)).findById(idNotaNaoExistente);
+    }
+    @Test
+    @DisplayName("Atualizar - atualiza uma nota ja cadastrada")
+    public void testAtualizarNota(){
+
+
+
+
     }
 }
