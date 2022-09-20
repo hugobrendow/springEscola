2eed6c118fbaa03d74f66a86d0c6fcdff40be327
Jackson Braga Ext (Objective) <jackson.braga.ext@sodexo.com>
2022-09-20 00:46:19 -0300

    Adicionado Spring security

---
+++ src/main/java/com/itau/escolaItauSpring/security/SecuityConfiguration.java
@@ -0,0 +1,23 @@
+package com.itau.escolaItauSpring.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecuityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests()
+                    .anyRequest().authenticated().and()
+                .httpBasic();
+        return http.build();
+    }
+}
