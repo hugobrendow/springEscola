4536ac3e6647a000c715b0cc66aa55db6b94cade
Davi Carrer <vittidavii@gmail.com>
2022-09-20 21:01:47 -0300

    Teste unitário testeMatricular

--- src/test/java/com/itau/escolaItauSpring/service/MatriculaServiceTest.java
+++ src/test/java/com/itau/escolaItauSpring/service/MatriculaServiceTest.java
@@ -1,24 +1,30 @@
 package com.itau.escolaItauSpring.service;
 
+import com.itau.escolaItauSpring.dto.request.MatriculaRequest;
+import com.itau.escolaItauSpring.dto.response.MatriculaResponse;
 import com.itau.escolaItauSpring.mapper.MatriculaMapper;
 import com.itau.escolaItauSpring.model.Aluno;
 import com.itau.escolaItauSpring.model.Matricula;
 import com.itau.escolaItauSpring.model.Turma;
 import com.itau.escolaItauSpring.repository.MatriculaRepository;
 import org.hibernate.mapping.Any;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
 
-@ExtendWith(MockitoExtension.class)
+
 class MatriculaServiceTest {
     @InjectMocks
     private MatriculaService matriculaService;
@@ -32,40 +38,59 @@ class MatriculaServiceTest {
     @Mock
     private MatriculaMapper mapper;
 
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        carregarClasses();
+    }
     @Test
-    void matricular() {
+    void testeMatricular() {
         Mockito.when(alunoService.buscarModelPorId(any())).thenReturn(new Aluno());
         Mockito.when(turmaService.buscarModelPorId(any())).thenReturn(new Turma());
 
-        Mockito.when(matriculaRepository.findByAlunoIdAndTurmaId(any(),any())).thenReturn(Optional.of(new Matricula()));
+        Mockito.when(matriculaRepository.findByAlunoIdAndTurmaId(any(),any())).thenReturn(Optional.empty());
         Mockito.when(turmaService.verificarSeHaVagas(any())).thenReturn(true);
+
+        Mockito.when(matriculaRepository.save(any())).thenReturn(new Matricula());
+        Mockito.doNothing().when(turmaService).atualizarVagas(any());
+
+        Mockito.when(mapper.toResponse(any())).thenReturn(new MatriculaResponse());
+
+        var resultadoTeste = matriculaService.matricular(new MatriculaRequest());
+        Assertions.assertInstanceOf(MatriculaResponse.class, resultadoTeste);
+
+        Mockito.verify(turmaService,times(1)).atualizarVagas(any());
     }
 
     @Test
-    void buscarModelPorId() {
+    void testeBuscarModelPorId() {
     }
 
     @Test
-    void buscarPorId() {
+    void testeBuscarPorId() {
     }
 
     @Test
-    void listarPorTurma() {
+    void testeListarPorTurma() {
     }
 
     @Test
-    void listarPorAluno() {
+    void testeListarPorAluno() {
     }
 
     @Test
-    void trancarMatricula() {
+    void testeTrancarMatricula() {
     }
 
     @Test
-    void ativarMatricula() {
+    void testeAtivarMatricula() {
     }
 
     @Test
-    void cancelarMatricula() {
+    void testeCancelarMatricula() {
+    }
+
+    void carregarClasses(){
+        Matricula matricula = new Matricula();
     }
 }
