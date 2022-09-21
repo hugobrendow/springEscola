ba45f67b213989509469734d996bbd33652687ee
Ana Luísa Fogarin <ana.luisa.fogarin@gmail.com>
2022-09-21 13:45:32 -0300

    test: adicionando testes de integração e unitários CursoDisciplinaService

--- src/test/java/com/itau/escolaItauSpring/service/CursoDisciplinaServiceIntegrationTest.java
+++ src/test/java/com/itau/escolaItauSpring/service/CursoDisciplinaServiceIntegrationTest.java
@@ -11,8 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDate;
-
 @SpringBootTest
 public class CursoDisciplinaServiceIntegrationTest {
 
