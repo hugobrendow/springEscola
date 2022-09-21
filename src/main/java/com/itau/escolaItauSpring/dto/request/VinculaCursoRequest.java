943779f63c1480c9006488a19bb624cb54288d38
Ana Luísa Fogarin <ana.luisa.fogarin@gmail.com>
2022-09-21 13:43:35 -0300

    fix: ajustando builder em VinculaCursoRequest

--- src/main/java/com/itau/escolaItauSpring/dto/request/VinculaCursoRequest.java
+++ src/main/java/com/itau/escolaItauSpring/dto/request/VinculaCursoRequest.java
@@ -1,5 +1,6 @@
 package com.itau.escolaItauSpring.dto.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +9,7 @@ import java.util.UUID;
 
 @Getter
 @Setter
+@Builder
 public class VinculaCursoRequest {
 
     @NotNull
